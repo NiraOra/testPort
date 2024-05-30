1926d595731936b55c9314e4918dc40134909bf8
n.arun_menon@student.unsw.edu.au <z5417727@ad.unsw.edu.au>
2024-05-30 14:54:05 +1000

    added stuff so far

--- footer.php
+++ footer.php
@@ -4,15 +4,15 @@
             <?php
             // Define your social media profile links
             $githubLink = "https://github.com/NiraOra";
-            $linkedinLink = "https://www.linkedin.com/in/NiranjanaArunMenon";
-            $replitLink = "https://replit.com/@yourusername";
+            $linkedinLink = "https://www.linkedin.com/in/niranjana-arun-menon";
+            $replitLink = "https://replit.com/@NiranjanaArun1";
             $gmailLink = "mailto:niraarunmenon@gmail.com";
 
             // Output the icons with links
-            echo "<a href='$githubLink'><img src='footerIcon/github.png' alt='GitHub'></a>";
-            echo "<a href='$linkedinLink'><img src='footerIcon/linkedin.png' alt='LinkedIn'></a>";
-            echo "<a href='$replitLink'><img src='footerIcon/replit.png' alt='repl.it'></a>";
-            echo "<a href='$gmailLink'><img src='footerIcon/gmail.png' alt='Gmail'></a>";
+            echo "<a href='$githubLink'><img src='footerIcon/github.png' width='20' height='20' padding='100' alt='GitHub'></a>";
+            echo "<a href='$linkedinLink'><img src='footerIcon/linkedin.png' width='20' height='20' alt='LinkedIn'></a>";
+            echo "<a href='$replitLink'><img src='footerIcon/replit.png' width='50' height='20' alt='repl.it'></a>";
+            echo "<a href='$gmailLink'><img src='footerIcon/gmail.png' width='20' height='20' alt='Gmail'></a>";
             ?>
         </div>
     </footer>
